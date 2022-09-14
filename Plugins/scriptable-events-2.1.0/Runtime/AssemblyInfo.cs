66dec1a13f6e91dba50c4e311fbfd5a68b6a075c
gk_unity <gulam@butterflyfields.com>
2022-09-15 00:23:05 +0530

    Unity atom added

--- Plugins/scriptable-events-2.1.0/Runtime/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly:InternalsVisibleTo("Chark.ScriptableEvents.Editor")]
-
-[assembly:InternalsVisibleTo("Chark.ScriptableEvents.Editor.Tests")]
-[assembly:InternalsVisibleTo("Chark.ScriptableEvents.Tests")]
